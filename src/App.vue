738cc91bd108584e6435eb675b20a8dfcffc265c
codwerk <louis_widdy@live.fr>
2020-06-04 11:56:37 +0200

    implementation of the header structure html/css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/portfolio">Portfolio</router-link>
+    <header>
+      <nav class="navbar">
+        <div class="logo"></div>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+          <li><router-link to="/portfolio">Portfolio</router-link></li>
+        </ul>
+      </nav>
+    </header>
 
     <router-view></router-view>
   </div>
@@ -19,13 +26,41 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url('assets/scss/index.scss');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
+  header {
+    padding: 10px;
+    .navbar {
+      max-width: 80%;
+      margin: auto;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .logo {
+        width: 50px;
+        height: 50px;
+        background: rgb(59, 73, 223);
+        border-radius: 50%;
+      }
+      ul {
+        display: flex;
+
+        li {
+          margin: 5px 15px;
+          white-space: nowrap;
+
+          a{
+            text-decoration: none;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
